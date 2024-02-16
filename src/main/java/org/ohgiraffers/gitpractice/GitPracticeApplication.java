b7af958068114c1f1483829fde768f6ed3598f12
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-02-16 10:45:33 +0900

    first commit

--- src/main/java/org/ohgiraffers/gitpractice/GitPracticeApplication.java
+++ src/main/java/org/ohgiraffers/gitpractice/GitPracticeApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitPracticeApplication {
 
+
     public static void main(String[] args) {
+        System.out.println("hello world!");
         SpringApplication.run(GitPracticeApplication.class, args);
     }
 
